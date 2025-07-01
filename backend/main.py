36a4ff01b0a44d236262708562fcbf421b24144c
Aniket Bhosale <aniket.bhosale22@pccoepune.org>
2025-07-01 19:05:20 +0530

    only one domain

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ origins = os.getenv("CORS_ORIGINS", "*").split(",")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[origin.strip() for origin in origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
